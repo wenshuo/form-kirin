60d1e9b916f953fface7ba3b8290c0e0a2496ffa
wenshuo huang <wen@brightbytes.net>
2020-01-09 16:02:57 -0800

    update script

--- scripts/release.js
+++ scripts/release.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
 
+execSync('rm -rf dist');
 execSync('NODE_ENV=production npm run build:prod');
 execSync('npm pack');
 execSync(`npm publish form-kirin-${npm_package_version}.tgz`);
